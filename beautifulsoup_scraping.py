3498d7b83ccc8b25e2074d06d3bba456c5e808fb
David Vélez <davidf.velez@udea.edu.co>
2024-03-30 18:54:52 -0500

    Selenium scraping

--- beautifulsoup_scraping.py
+++ beautifulsoup_scraping.py
@@ -1,37 +1,30 @@
-# -*- coding: utf-8 -*-
-"""
-  proyecto-data-science-2022-fifa-world-cup
-
-"""
-
 import pandas as pd
 from string import ascii_uppercase as alfabeto
 import pickle
+from bs4 import BeautifulSoup
+import requests
+
 
 #Extracción de grupos
 tables = pd.read_html('https://web.archive.org/web/20221115040351/https://en.wikipedia.org/wiki/2022_FIFA_World_Cup')
 
-dict_grupos = {}
+dict_groups = {}
 for letra, i in zip(alfabeto, range(12, 68, 7)):
   #print(i)
   df = tables[i]
   df.rename(columns={df.columns[1]: 'Team'}, inplace= True)
   df.set_index('Pos', inplace=True)
   df.pop('Qualification')
-  dict_grupos[f'Grupo {letra}'] = df
+  dict_groups[f'Grupo {letra}'] = df
 #dict_grupos.keys()
-dict_grupos['Grupo A']
+dict_groups['Grupo A']
 
 #Exportar el diccionario
 with open('dict_grupos', 'wb') as output:
-  pickle.dump(dict_grupos, output)
-
-from bs4 import BeautifulSoup
-import requests
+  pickle.dump(dict_groups, output)
 
 #Extraccion de informacion de los partidos jugados en mundiales anteriores
-years = [1930, 1934, 1938, 1942, 1946, 1950, 1954, 1958, 1962, 1966, 1970, 1974, 1978, 1982, 1986, 1990, 1994, 1998, 2002, 2006, 2010, 2014, 2018]
-
+years = [1930, 1934, 1938, 1950, 1954, 1958, 1962, 1966, 1970, 1974, 1978, 1982, 1986, 1990, 1994, 1998, 2002, 2006, 2010, 2014, 2018]
 
 def get_games(year):
   page = f'https://en.wikipedia.org/wiki/{year}_FIFA_World_Cup'
@@ -61,7 +54,5 @@ def get_games(year):
 
 # record of fifa world cup matches
 fifa = [get_games(year) for year in years]
-
 df_cups = pd.concat(fifa, ignore_index= True)
-
 df_cups.to_csv('fifa_cups.csv', index = False)
---
+++ selenium_scraping.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import pandas as pd
+import time
+
+path = r'C:\Users\dvelezc\Downloads\chromedriver.exe'
+service = Service(executable_path=path)
+driver = webdriver.Chrome(service=service)
+
+#Extraccion de informacion de los partidos jugados en mundiales anteriores
+years = [1930, 1934, 1938, 1950, 1954, 1958, 1962, 1966, 1970, 1974, 1978, 1982, 1986, 1990, 1994, 1998, 2002, 2006, 2010, 2014, 2018]
+def get_games(year):
+    web = f'https://en.wikipedia.org/wiki/{year}_FIFA_World_Cup'
+    driver.get(web)
+    games = driver.find_elements(by='xpath', value='//th[@class="fhome"]/..')
+
+    home = []
+    score = []
+    away = []
+
+    for match in games:
+        home.append(match.find_element(by='xpath', value='./th[1]').text)
+        score.append(match.find_element(by='xpath', value='./th[2]').text)
+        away.append(match.find_element(by='xpath', value='./th[3]').text)
+
+    print(f'{len(home)} {len(score)} {len(away)}')
+
+    dict_fifa = {'home': home, 'score': score, 'away': away}
+
+    df_cups = pd.DataFrame(dict_fifa)
+    df_cups['year'] = year
+    time.sleep(2)
+    #print(df_cups)
+    return df_cups
+
+
+
+df_for_year = [get_games(year) for year in years]
+df_fifa = pd.concat(df_for_year)
+df_fifa.to_csv('fifa_cups.csv', index = False )
+driver.quit()
