f65e260e066aa2770ccdf0809a830193a474f5b1
David Vélez <davidf.velez@udea.edu.co>
2024-03-30 23:23:26 -0500

    Data cleaning and preparation

--- beautifulsoup_scraping.py
+++ beautifulsoup_scraping.py
@@ -24,7 +24,7 @@ with open('dict_grupos', 'wb') as output:
   pickle.dump(dict_groups, output)
 
 #Extraccion de informacion de los partidos jugados en mundiales anteriores
-years = [1930, 1934, 1938, 1950, 1954, 1958, 1962, 1966, 1970, 1974, 1978, 1982, 1986, 1990, 1994, 1998, 2002, 2006, 2010, 2014, 2018]
+years = [1930, 1934, 1938, 1950, 1954, 1958, 1962, 1966, 1970, 1974, 1978, 1982, 1986, 1990, 1994, 1998, 2002, 2006, 2010, 2014, 2018, 2022]
 
 def get_games(year):
   page = f'https://en.wikipedia.org/wiki/{year}_FIFA_World_Cup'
